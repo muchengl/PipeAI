aa84ca84cf7ea5a09dcdda8f73d88d4f99204c6e
Andrea Liu <liuhanzhong.lhz@gmail.com>
2024-01-06 19:36:39 -0600

    Init

---
+++ src/main/java/org/aipipe/AipipeApplication.java
@@ -0,0 +1,13 @@
+package org.aipipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AipipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aipipe/AipipeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aipipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
